597ac94ee6149f706a8bc1ede7dd8849ed80b60b
Matthew Echert <mgechert@gmail.com>
2023-12-05 12:28:59 -0700

    Day 1 completed solution

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Tuple
+# from typing_extensions import Self
 
 # Advent of Code Day 1 solution code
 
 
 DIGITS = set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-WORD_SUBS = {
+WORDS_DIGITS = {
     "one": "1",
     "two": "2",
     "three": "3",
@@ -26,7 +27,8 @@ def calibrate_line(line: str) -> int:
 
     return value
 
-def calibrate_document(document: List[str]):
+
+def calibrate_document(document: List[str]) -> int:
     calibration_total = 0
 
     for line in document:
@@ -36,9 +38,100 @@ def calibrate_document(document: List[str]):
     return calibration_total
 
 # Part 2
-def calibrate_line_v2(line: str) -> int:
-    for word, sub in WORD_SUBS.items():
-        line = line.replace(word, sub)
 
-    return calibrate_line(line)
 
+class Trie:
+
+    class Node:
+        def __init__(self) -> None:
+            self.children = {}
+            self.matching_word = None
+
+        def get_child(self, key: str):
+            if key not in self.children:
+                self.children[key] = self.__class__()
+
+            return self.children[key]
+
+    def __init__(self) -> None:
+        self.root = self.Node()
+
+    def insert(self, word: str) -> None:
+        """Insert a word to the trie"""
+        if len(word) == 0:
+            return
+
+        self._insert(self.root, word, 0)
+
+    def _insert(self, node: Node, word: str, index: int) -> None:
+        """Recursive insert"""
+        child = node.get_child(word[index])
+
+        if index == len(word) - 1:
+            child.matching_word = word
+        else:
+            self._insert(child, word, index + 1)
+
+    def print(self) -> None:
+        self._print(self.root)
+
+    def _print(self, node: Node, prefix="") -> None:
+        """Recursive print"""
+        if node.matching_word:
+            print(f"******WORD: {node.matching_word}")
+
+        for key, child_node in node.children.items():
+            display_str = prefix + key + "-"
+            print(display_str)
+            self._print(child_node, display_str)
+
+    def search(self, search_string: str) -> Tuple[str, str]:
+        """Scans the search string recursively for a match"""
+        return self._search(self.root, search_string, 0)
+
+    def _search(self, node: Node, search_string: str, index: int) -> str:
+        """Recursive search"""
+        if index < len(search_string) and search_string[index] in node.children:
+            child_node = node.children[search_string[index]]
+            child_result = self._search(child_node, search_string, index + 1)
+            # Return the child result if there is one, otherwise this node (may be None)
+            return child_result or node.matching_word
+        else:
+            return node.matching_word
+
+    def find_all(self, search_string: str) -> List[str]:
+        """Find all matches inside search_string"""
+        results = []
+        i = 0
+
+        while i < len(search_string):
+            result = self.search(search_string[i:])
+            if result:
+                results.append(result)
+            i = i + 1
+
+        return results
+
+
+def scan_document(document: List[str]) -> List[int]:
+    """Scans every line in the list and returns the
+    int result of the first and last digits found in each line"""
+    trie = Trie()
+    results = []
+
+    for word, digit in WORDS_DIGITS.items():
+        trie.insert(word)
+        trie.insert(digit)
+
+    for line in document:
+        line_results = trie.find_all(line)
+        left, right = line_results[0], line_results[-1]
+
+        # Replace words with digits where applicable
+        left = WORDS_DIGITS.get(left, left)
+        right = WORDS_DIGITS.get(right, right)
+
+        int_result = int(left + right)
+        results.append(int_result)
+
+    return results
--- 2023/tests/test_day01.py
+++ 2023/tests/test_day01.py
@@ -1,15 +1,16 @@
 import pytest
 
 from aoc_utils import parse_input_file
-from day01 import calibrate_line, calibrate_document, calibrate_line_v2
+from day01 import calibrate_line, calibrate_document, Trie, scan_document
 
 
 @pytest.fixture
 def example_input():
     return parse_input_file("day01ex.txt")
 
+
 @pytest.fixture
-def part1_input():
+def problem_input():
     return parse_input_file("day01.txt")
 
 
@@ -21,19 +22,12 @@ def test_example_reads_as_expected(example_input):
     assert actual_len == expected_len
 
 
-def test_part1_input_is_expected_len(part1_input):
-    actual_len = len(part1_input)
+def test_problem_input_is_expected_len(problem_input):
+    actual_len = len(problem_input)
     expected_len = 1000
 
-    assert type(part1_input) is list
+    assert type(problem_input) is list
     assert actual_len == expected_len
-
-
-def test_calibrate_line_reads_line_with_2_digits():
-    value = calibrate_line("1abc2")
-    expected_value = 12
-
-    assert value == expected_value
 
 
 def test_calibrate_line_reads_line_with_2_digits():
@@ -70,8 +64,8 @@ def test_calibrate_example_document_returns_expected(example_input):
     assert example_solution == expected_solution
 
 
-def test_part1_input(part1_input):
-    part1_solution = calibrate_document(part1_input)
+def test_problem_input(problem_input):
+    part1_solution = calibrate_document(problem_input)
     expected_solution = 54634  # Accepted p1 solution
 
     print(f"Part 1 solution: {part1_solution}")
@@ -80,7 +74,8 @@ def test_part1_input(part1_input):
 
 
 # Part 2
-def test_format_line_handles_examples():
+@pytest.fixture
+def ex2_input():
     ex2_input = [
         "two1nine",
         "eightwothree",
@@ -90,8 +85,74 @@ def test_format_line_handles_examples():
         "zoneight234",
         "7pqrstsixteen"
     ]
-    ex_values = [calibrate_line_v2(line) for line in ex2_input]
+
+    return ex2_input
+
+
+def test_trie_basic_insert_and_search():
+    trie = Trie()
+
+    test_words = ["six", "sixteen", "sixty", "seven"]
+    for word in test_words:
+        trie.insert(word)
+
+    for word in test_words:
+        result = trie.search(word)
+
+        print(f"Searched for {word} got {result}")
+        assert result == word
+
+    expected_none = trie.search("notintrie")
+
+    assert expected_none is None
+
+
+def test_trie_find_all():
+    trie = Trie()
+    test_words = ["six", "sixteen", "sixty", "seven"]
+    for word in test_words:
+        trie.insert(word)
+
+    results = trie.find_all("sixteenthenseventhensix")
+
+    expected_results = ["sixteen", "seven", "six"]
+
+    assert results == expected_results
+
+
+def test_scan_document_handles_examples(ex2_input):
+    ex_results = scan_document(ex2_input)
+    ex_sum = sum(ex_results)
 
     expected_values = [29, 83, 13, 24, 42, 14, 76]
+    expected_sum = 281
 
-    assert ex_values == expected_values
+    assert ex_results == expected_values
+    assert ex_sum == expected_sum
+
+
+def test_scan_document_handles_edge_case():
+    """Test a fix for this edge case found in the problem input:
+    szsvltgsc1onecccbfour3oneightfh
+    (Was returning 11 instead of 18)
+    """
+    result = scan_document(["szsvltgsc1onecccbfour3oneightfh"])
+
+    expected_result = [18]
+
+    assert result == expected_result
+
+
+def test_part2_input(problem_input):
+    part2_result = scan_document(problem_input)
+    part2_solution = sum(part2_result)
+
+    for i in range(100):
+        print(f"{problem_input[i]}: {part2_result[i]}")
+
+    # Tried: 53846 (low)
+    accepted_solution = 53855  # Accepted p2 solution
+
+    print(f"Part 2 solution: {part2_solution}")
+
+    assert part2_solution == accepted_solution
