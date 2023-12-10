9c5c88bbd1e0151a3da6450344031df2d5d6547b
Matthew Echert <mgechert@gmail.com>
2023-12-09 18:57:20 -0700

    Add a helper to read raw string inputs

--- 2023/aoc_utils/aoc_utils.py
+++ 2023/aoc_utils/aoc_utils.py
@@ -22,6 +22,23 @@ def parse_input_file(filename: str,
     return output
 
 
+
+def read_raw_input_file(filename: str,
+                     inputs_path: str = None) -> str:
+    """
+    Read the given input file into a single string
+    """
+    inputs_path = inputs_path or os.environ.get("AOC_INPUTS_PATH", None)
+    if not inputs_path:
+        raise EnvironmentError("AOC_INPUTS_PATH not set in os.environ,"
+                               "and inputs_path was not explicitly specified")
+
+    with open(f"{inputs_path}/{filename}") as f:
+        output = f.read()
+
+    return output
+
+
 def get_adjacent_points(row: int, col: int, num_rows: int, num_cols: int) -> List[Tuple[int, int]]:
     """Given a point (r, c), return a list of up to 8 adjacent points based on a
         2D array of size num_rows x num_cols, filtering out indices that would be
